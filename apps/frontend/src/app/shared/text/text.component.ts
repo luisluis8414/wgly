87daaf0eeb8300a987068ec73c35d375b74657d2
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-10-29 16:55:00 +0100

    text

---
+++ apps/frontend/src/app/shared/text/text.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wgplanner-text',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.scss'],
+})
+export class TextComponent {}
