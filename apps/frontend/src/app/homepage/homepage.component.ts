cbef3984401dab7ab4909ba102acf2dec675006e
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-10-29 15:51:06 +0100

    Created homepage

---
+++ apps/frontend/src/app/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wgplanner-homepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss'],
+})
+export class HomepageComponent {}
