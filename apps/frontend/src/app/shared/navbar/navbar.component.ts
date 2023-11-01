cbd84c69ad26d440f49b23891bf65cb959c9aa2b
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-11-01 13:34:13 +0100

    Added animations to sidebar

--- apps/frontend/src/app/shared/navbar/navbar.component.ts
+++ apps/frontend/src/app/shared/navbar/navbar.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextComponent } from '../text/text.component';
 import { IconComponent } from '../icon/icon.component';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'wgplanner-navbar',
@@ -9,6 +16,30 @@ import { IconComponent } from '../icon/icon.component';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   imports: [CommonModule, TextComponent, IconComponent],
+  animations: [
+    trigger('sidebarAnimation', [
+      state(
+        'collapsed',
+        style({
+          width: '0',
+          opacity: 0,
+          overflow: 'hidden',
+          display: 'none',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          width: '30vw',
+          opacity: 1,
+          overflow: 'hidden',
+          display: 'block',
+        })
+      ),
+      transition('collapsed => expanded', animate('200ms ease-in-out')),
+      transition('expanded => collapsed', animate('100ms ease-out')),
+    ]),
+  ],
 })
 export class NavbarComponent {
   protected sidebarCollapsed = true;
