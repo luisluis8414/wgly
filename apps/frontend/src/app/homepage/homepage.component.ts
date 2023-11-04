d1a6ce63e2e408b8238133d3f0d3891da80eb289
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-11-04 14:41:21 +0100

    Test Api Call

--- apps/frontend/src/app/homepage/homepage.component.ts
+++ apps/frontend/src/app/homepage/homepage.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../shared/icon/icon.component';
-import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { AppService } from '../app.service';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'wgplanner-homepage',
   standalone: true,
-  imports: [CommonModule, IconComponent, NavbarComponent],
+  imports: [CommonModule, IconComponent, SidebarComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent {}
+export class HomepageComponent implements OnInit {
+  private appService = inject(AppService);
+  protected message!: string;
+
+  ngOnInit(): void {
+    this.appService.getHello().subscribe((data) => {
+      this.message = data.message;
+    });
+  }
+}
