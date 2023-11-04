cc1f89d73bbc55d9f5f285536cc6b3b4802d1b85
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-11-04 14:40:34 +0100

    enabled cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,16 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
