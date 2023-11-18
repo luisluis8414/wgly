bda9dea062ab2a5809d106fbfa05f2629149fe96
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-11-18 13:32:46 +0100

    Discord Button Animations

---
+++ apps/frontend/src/app/shared/discord-login/discord-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'wgly-discord-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './discord-login.component.html',
+  styleUrls: ['./discord-login.component.scss'],
+})
+export class DiscordLoginComponent {}
