661d2ade57b2514f691610f23e2bbd78e6a1f64b
Luis Wehrberger <luis.wehrberger2@gmail.com>
2023-11-19 12:47:24 +0100

    Fixed failing frontend

--- apps/frontend/src/app/authpage/authpage.component.spec.ts
+++ apps/frontend/src/app/authpage/authpage.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { AuthpageComponent } from './authpage.component'
 import { ActivatedRoute } from '@angular/router'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
 
 describe('AuthpageComponent', () => {
   let component: AuthpageComponent
@@ -9,7 +10,7 @@ describe('AuthpageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthpageComponent, BrowserAnimationsModule],
+      imports: [AuthpageComponent, BrowserAnimationsModule, HttpClientModule],
       providers: [
         {
           provide: ActivatedRoute,
