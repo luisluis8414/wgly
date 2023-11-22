cbb8f04555425e6d23535d9a04346f86a1c5cb11
Luis Wehrberger <luis.wehrberger@iteratec.com>
2023-11-22 10:39:00 +0100

    ShoppingList:  Input

--- apps/frontend/src/app/shoppinglist/shoppinglist.component.ts
+++ apps/frontend/src/app/shoppinglist/shoppinglist.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SidebarComponent } from '../shared/sidebar/sidebar.component'
+import { InputComponent } from '../shared/input/input.component'
 
 @Component({
   selector: 'wgly-shoppinglist',
   standalone: true,
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.scss'],
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, InputComponent],
 })
 export class ShoppinglistComponent {}
