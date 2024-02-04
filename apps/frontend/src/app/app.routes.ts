0f95cc881e2d1435f2213d8db9c5bfb4c2746d62
luisluis84 <luis.wehrberger2@gmail.com>
2024-02-04 18:08:48 +0100

    [main] switched back to postgres

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthpageComponent } from './authpage/authpage.component'
 import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component'
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'shopping', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'auth', component: AuthpageComponent },
   { path: 'shopping', component: ShoppinglistComponent },
