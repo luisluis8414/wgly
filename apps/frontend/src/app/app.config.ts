9055174ff2d23a8976e264ed7fc8730012c7e1f2
Luis Wehrberger <luis.wehrberger@iteratec.com>
2024-02-16 15:40:04 +0100

    pwa:  enable service worker and pwa

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,16 +1,21 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, isDevMode } from '@angular/core'
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router'
 import { appRoutes } from './app.routes'
 import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideHttpClient(),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 }
